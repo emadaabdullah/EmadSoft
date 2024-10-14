2dc40a86ff7908f282350993bcee67bee231ed29
emadaabdullah <143230658+emadaabdullah@users.noreply.github.com>
2024-10-14 03:26:02 +0300

    Create Test.py
    محمد رسول الله

---
+++ Test.py
@@ -0,0 +1,7 @@
+echo "# EmadSoft" >> README.md 
+git init 
+git add README.md 
+git commit -m "first commit" 
+git branch -M main 
+git remote add origin https://github.com/emadaabdullah/EmadSoft.git
+ git push -u origin main
